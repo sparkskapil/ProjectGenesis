afe4bf87539b5ed82e3a14054f15f5bba82ab10c
Kapil Verma <sparkskapil@gmail.com>
2023-03-05 16:38:14 +0530

    [Vulkan] Setup Validation Layers

--- code/assert.h
+++ code/assert.h
@@ -4,4 +4,8 @@
 
 #define genAssertDebug(condition, message) if(!(condition))log_warn(message); DEBUG_ASSERT(condition);
 #define genAssert(condition, message) if(!(condition))log_error(message); ASSERT(condition);
-#define genCriticalAssert(condition, message) if(!(condition))log_fatal(message); VERIFY(condition);
+#define genAssertCritical(condition, message) if(!(condition))log_fatal(message); VERIFY(condition);
+
+#define genFailedDebug(message) genAssertDebug(false, message);
+#define genFailed(message) genAssert(false, message);
+#define genFailedCritical(message) genAssertCritical(false, message);
--- code/main.cpp
+++ code/main.cpp
@@ -10,6 +10,15 @@
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
+const std::vector<const char*> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"
+};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
 
 class Genesis
 {
@@ -30,12 +39,13 @@ public:
             SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
             WIDTH, HEIGHT,
             SDL_WINDOW_ALLOW_HIGHDPI | SDL_WINDOW_VULKAN);
-        genCriticalAssert(m_Window != nullptr, L"Could not create SDL window.");
+        genAssertCritical(m_Window != nullptr, L"Could not create SDL window.");
     }
 
     void initVulkan()
     {
         createInstance();
+        setupDebugMessenger();
     }
 
     void mainLoop()
@@ -53,17 +63,27 @@ public:
 
     void cleanup()
     {
+        if (enableValidationLayers)
+        {
+            DestroyDebugUtilsMessengerEXT(m_Instance, m_DebugMessenger, nullptr);
+        }
+        vkDestroyInstance(m_Instance, nullptr);
         SDL_DestroyWindow(m_Window);
         SDL_Quit();
     }
 
     void createInstance()
     {
+        if (enableValidationLayers && !checkValidationLayerSupport())
+        {
+            genFailedCritical(L"Validation layers requested, but not available!");
+        }
+
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.pApplicationName = "Genesis";
         appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-        appInfo.pEngineName = "No Engine";
+        appInfo.pEngineName = "Genesis";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
 
@@ -71,23 +91,121 @@ public:
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
+        // Handle Extensions
         uint32_t sdlExtensionCount = 0;
         SDL_Vulkan_GetInstanceExtensions(m_Window, &sdlExtensionCount, NULL);
 
         std::vector<const char*> sdlExtensions(sdlExtensionCount);
         SDL_Vulkan_GetInstanceExtensions(m_Window, &sdlExtensionCount, sdlExtensions.data());
+        
+        if (enableValidationLayers)
+        {
+            sdlExtensions.push_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
+        }
 
-        createInfo.enabledExtensionCount = sdlExtensionCount;
+        createInfo.enabledExtensionCount = (uint32_t)sdlExtensions.size();
         createInfo.ppEnabledExtensionNames = sdlExtensions.data();
 
         createInfo.enabledLayerCount = 0;
 
-        genCriticalAssert(vkCreateInstance(&createInfo, nullptr, &m_Instance) == VK_SUCCESS, L"Failed to create instance!");
+        // Handle Layers
+        if (enableValidationLayers)
+        {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else
+        {
+            createInfo.enabledLayerCount = 0;
+        }
+
+        genAssertCritical(vkCreateInstance(&createInfo, nullptr, &m_Instance) == VK_SUCCESS, L"Failed to create instance!");
+    }
+
+    bool checkValidationLayerSupport()
+    {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        for (const char* layerName : validationLayers)
+        {
+            bool layerFound = false;
+
+            for (const auto& layerProperties : availableLayers)
+            {
+                if (strcmp(layerName, layerProperties.layerName) == 0)
+                {
+                    layerFound = true;
+                    break;
+                }
+            }
+
+            if (!layerFound)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    void setupDebugMessenger()
+    {
+        VkDebugUtilsMessengerCreateInfoEXT createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
+        createInfo.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+        createInfo.messageType = VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+        createInfo.pfnUserCallback = Genesis::debugCallback;
+        createInfo.pUserData = nullptr; // Optional
+        if (CreateDebugUtilsMessengerEXT(m_Instance, &createInfo, nullptr, &m_DebugMessenger) != VK_SUCCESS)
+        {
+            genFailed(L"Failed to set up debug messenger!");
+        }
+    }
+
+    VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo, const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger)
+    {
+        auto func = (PFN_vkCreateDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkCreateDebugUtilsMessengerEXT");
+        if (func != nullptr)
+        {
+            return func(instance, pCreateInfo, pAllocator, pDebugMessenger);
+        }
+        else
+        {
+            return VK_ERROR_EXTENSION_NOT_PRESENT;
+        }
+    }
+
+    void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator)
+    {
+        auto func = (PFN_vkDestroyDebugUtilsMessengerEXT)vkGetInstanceProcAddr(instance, "vkDestroyDebugUtilsMessengerEXT");
+        if (func != nullptr)
+        {
+            func(instance, debugMessenger, pAllocator);
+        }
+    }
+
+public:
+    static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(
+        VkDebugUtilsMessageSeverityFlagBitsEXT messageSeverity,
+        VkDebugUtilsMessageTypeFlagsEXT messageType,
+        const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+        void* pUserData)
+    {
+
+        std::cerr << "validation layer: " << pCallbackData->pMessage << std::endl;
+
+        return VK_FALSE;
     }
 
 private:
     SDL_Window* m_Window;
     VkInstance m_Instance;
+
+    VkDebugUtilsMessengerEXT m_DebugMessenger;
 };
 
 int main()
